a4239347a43146cec1cee6eecad6609b95bedd75
prashantkr2601 <prashantkr2601@gmail.com>
2023-07-23 08:46:40 +0530

    deployment changes

--- src/config/config.js
+++ src/config/config.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDh377wz1P0ISt_Qht-AY7smme9dB6XycA",
-  authDomain: "testdeltaexchange-6ea5f.firebaseapp.com",
-  projectId: "testdeltaexchange-6ea5f",
-  storageBucket: "testdeltaexchange-6ea5f.appspot.com",
-  messagingSenderId: "276939666943",
-  appId: "1:276939666943:web:32fbbedb4fffc4594ba3ca",
-  measurementId: "G-0JEPDQXJCM",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
