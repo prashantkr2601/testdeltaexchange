f352452aace9fb52dbb15492dc34819621b54da0
prashantkr2601 <prashantkr2601@gmail.com>
2023-07-23 09:05:13 +0530

    added spi token

--- src/config/config.js
+++ src/config/config.js
@@ -9,8 +9,8 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export default firebaseConfig;
